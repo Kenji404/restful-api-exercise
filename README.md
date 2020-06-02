97299d10923c89dd5d6767b59c2c6c11b65fde5a
Kenji404 <kenji.andre@gmail.com>
2020-06-02 15:11:21 -0300

    routes and middleware

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Developing a generic RESTful API using Javascript, Node.JS, Express.JS and MongoDB.
+
+following this tutorial https://www.youtube.com/watch?v=vjf774RKrLc&t=1203s
+
+stopped at: 8:56
+
+done:
+    - npm init
+    - npm install
+            Express
+            Nodemon
+    - Routes setup
+    - Middleware setup
+    
--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-
-
+//Middleware
+app.use('/link', () => {
+    console.log('/link');
+});
+//Routes
 app.get('/', (req, res) => {
     res.send('We are live!');
 });
 
+app.get('/link', (req, res) => {
+    res.send('We are live on /link');
+});
+//Port listen
 app.listen(3000);
